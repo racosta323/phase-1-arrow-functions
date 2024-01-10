af0ff37a8d912606ae7448d05abe1036cc3685b6
Rene <racosta323@gmail.com>
2024-01-09 23:17:14 -0600

    lab complete

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function (a,b){
+return a/b;
+}; //my function expression
+
+const square = x => x*x;
+
+const add = (a,b) => a+b;
+
+console.log(divide(2000, 100));
+console.log(square(2));
+console.log(add(3,4));
+
